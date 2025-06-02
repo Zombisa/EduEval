5d5ac37cdcf305d7d92ba1293d9b6e98bcd7134d
Isabela <isanchez@unicauca.edu.co>
2025-06-02 14:28:28 -0500

    CORS

--- edueval_backend/settings.py
+++ edueval_backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles', 
     'django_filters',
+    'corsheaders',
 
     'rest_framework',
     'apps.competencias_programa',
@@ -96,6 +97,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -108,6 +110,7 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'edueval_backend.urls'
 
 CSRF_TRUSTED_ORIGINS = ['http://localhost:8000']
+CORS_ALLOWED_ORIGINS = ['http://localhost:4200']
 
 TEMPLATES = [
     {
