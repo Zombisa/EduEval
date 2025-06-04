3995506feadb84df5d71608eaac547c54fd6dade
Isabela Sánchez Saavedra <isanchez@unicauca.edu.co>
2025-06-04 00:32:25 -0500

    small change to signal + instructions

--- apps/rubricas/models/signals.py
+++ apps/rubricas/models/signals.py
@@ -1,9 +1,16 @@
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
+from django.db import connection
 from .models import NivelDesempeno
+
+def table_exists(table_name):
+    return table_name in connection.introspection.table_names()
 
 @receiver(post_migrate)
 def crear_niveles_predeterminados(sender, **kwargs):
+    if not table_exists('rubricas_niveldesempeno'):
+        return 
+
     niveles = [
         (1, "Bajo"),
         (2, "Medio"),
